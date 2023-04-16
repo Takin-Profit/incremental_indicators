e676adf63902ed05b3aba28a611c0d68098a6c00
Gary Becks <42525743+g5becks@users.noreply.github.com>
2023-04-16 15:21:38 -0400

    feat(Numerix): add slope function
    add slope function to numerix module

--- src/numerix.ts
+++ src/numerix.ts
@@ -5,15 +5,16 @@ import { Either, Left, Right } from 'purify-ts'
 
 // Standard Deviation function
 export const stdDev = (values: number[]): Either<Error, number> => {
-  if (!values) {
+  // eslint-disable-next-line unicorn/no-null
+  if (values == null) {
     return Left(new Error('StdDev values cannot be null'))
   }
   let sd = 0
   const n = values.length
   if (n > 1) {
     let sum = 0
-    for (let index = 0; index < n; index++) {
-      sum += values[index]
+    for (let i = 0; i < n; i++) {
+      sum += values[i]
     }
     const avg = sum / n
     let sumSq = 0
@@ -24,4 +25,40 @@ export const stdDev = (values: number[]): Either<Error, number> => {
     sd = Math.sqrt(sumSq / n)
   }
   return Right(sd)
+}
+
+export const slope = (x: number[], y: number[]): Either<Error, number> => {
+  // eslint-disable-next-line unicorn/no-null
+  if (x == null) {
+    return Left(new Error('Slope X values cannot be null.'))
+  }
+  // eslint-disable-next-line unicorn/no-null
+  if (y == null) {
+    return Left(new Error('Slope Y values cannot be null'))
+  }
+  if (x.length !== y.length) {
+    return Left(new Error('Slope X and Y arrays must be the same size.'))
+  }
+  const length = x.length
+  let sumX = 0
+  let sumY = 0
+  for (let i = 0; i < length; i++) {
+    sumX += x[i]
+    sumY += y[i]
+  }
+  const avgX = sumX / length
+  const avgY = sumY / length
+
+  // least squares method
+  let sumSqX = 0
+  let sumSqXY = 0
+
+  for (let i = 0; i < length; i++) {
+    const devX = x[i] - avgX
+    const devY = y[i] - avgY
+
+    sumSqX += devX * devX
+    sumSqXY += devX * devY
+  }
+  return Right(sumSqXY / sumSqX)
 }
